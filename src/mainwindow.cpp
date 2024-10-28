e75cdb6aea5d2cb0baff6b718d1a03e411e240af
Mads <mathias.castren@aalto.fi>
2024-10-28 22:26:36 +0200

    Changed mainwindows line edit so it prints the input line.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "../ui/ui_mainwindow.h"
 #include <QMessageBox>
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +14,18 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_lineEdit_returnPressed()
+{
+    QLineEdit *lineEdit = qobject_cast<QLineEdit*>(sender());
+
+    if (lineEdit) {
+        QString input = lineEdit->text();  // Get the entered text
+
+        std::cout << input.toStdString() << std::endl;
+
+        lineEdit->clear();
+    }
+}
+
