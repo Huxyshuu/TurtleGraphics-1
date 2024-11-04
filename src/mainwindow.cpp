c5b7775941279752965debbf22329cfbdb0c2647
Huxyshuu <hugotamm@hotmail.com>
2024-11-04 20:26:12 +0200

    Mainwindow currently draws a temporary red circle in the center

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -24,9 +24,8 @@ MainWindow::MainWindow(QWidget *parent)
     // Add the item to the scene
     scene->addItem(turtle);
 
+    // Draws a red circle in the center of the screen
     QGraphicsEllipseItem *dot = scene->addEllipse(-5, -5, 10, 10, QPen(Qt::red), QBrush(Qt::red));
-
-
     scene->addItem(dot);
 
     ui->graphicsView->setScene(scene);
