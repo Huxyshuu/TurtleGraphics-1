e46f6ffda417cadd5357eed4c639eca7c485bc53
Huxyshuu <hugotamm@hotmail.com>
2024-11-04 15:04:03 +0200

    Added a few images of a turtle. Made a pixmap show up.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,13 +1,33 @@
 #include "mainwindow.h"
 #include "../ui/ui_mainwindow.h"
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QPixmap>
 #include <QMessageBox>
 #include <iostream>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QGraphicsScene *scene = new QGraphicsScene(this);
+
+    QPixmap turtlePixmap(":/assets/turtle_mid.png"); // Path copied from the .qrc file. Mid picture seems like the best size as a default.
+
+    if (!turtlePixmap.isNull()) {
+        QGraphicsPixmapItem *turtleItem = new QGraphicsPixmapItem(turtlePixmap);
+
+        // Add the item to the scene
+        scene->addItem(turtleItem);
+
+        ui->graphicsView->setScene(scene);
+    } else {
+        QMessageBox::warning(this, "Error", "Failed to load turtle image.");
+        delete scene;
+    }
 }
 
 MainWindow::~MainWindow()
@@ -28,4 +48,5 @@ void MainWindow::on_lineEdit_returnPressed()
         lineEdit->clear();
     }
 }
+
 
